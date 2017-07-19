28de764b3faa95b7098d3aceb5820ae7c1eaa405
Anton <anton.swifton@gmail.com>
2017-07-19 02:53:35 -0400

    Minor refactoring

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
 realtime = true;
-resizeCanvas();
+resize_canvas();
 
 var pixels_in_meter = 180; 
 var player_x = 25;
@@ -11,23 +11,45 @@ var speed_of_player_y = 0;
 var dt = 1 / 60;
 var gravity_acceleration = 10;
 var flying = true;
-var levelWidth = 1000; //
-var levelHeight = 150;//
-var levelMap = new Array(levelWidth);
-var blockPixelWidth = 20;
-var blockPixelHeight = 20;
-var blockMeterWid = blockPixelWidth / pixels_in_meter;
-var blockMeterHeit = blockPixelHeight / pixels_in_meter;
-var screenWidth = window.innerWidth - 10;
-var screenHeight = window.innerHeight - 16;
+var level_wid = 1000; //
+var level_heit = 150;//
+var level_map = new Array(level_wid);
+var block_pixel_wid = 20;
+var block_pixel_heit = 20;
+var block_meter_wid = block_pixel_wid / pixels_in_meter;
+var block_meter_heit = block_pixel_heit / pixels_in_meter;
+var screen_wid = main_canvas.width;
+var screen_heit = main_canvas.height;
 
-var floor = (canvas.height * (2 / 3)) / pixels_in_meter;
+function player_collides_with_map() {	
+	var left_top = player_left_top_block()
+	left_top_x_block = left_top[0];
+	left_top_y_block = left_top[1];
+	
+	var bottom_right = player_bottom_right_block();
+	bottom_right_x_block = bottom_right[0];
+	bottom_right_y_block = bottom_right[1];
+	
+	for (var i = left_top_x_block; i <= bottom_right_x_block; i++) {
+		for (var j = left_top_y_block; j <= bottom_right_y_block; j++) {
+			if (level_map[i] == undefined) {
+				
+			}
+			else if (level_map[i][j] == undefined) {
+				
+			}
+			else if (level_map[i][j] != 0) {return j * block_meter_heit;}
+		}
+	}
+	
+	return false;
+}
 
 function step() {
 	player_x += speed_of_player_x * dt;
 	player_y += speed_of_player_y * dt;
 	
-	var collision = Collision();
+	var collision = player_collides_with_map();
 	
 	if (collision) {
 		speed_of_player_y = 0;
@@ -40,29 +62,23 @@ function step() {
 	
 	speed_of_player_y += gravity_acceleration * dt;
 	
-	clear(mainCanvas, "#333333");
-	Draw();
+	clear_canvas(main_canvas, "#333333");
+	draw();
 }
 
-function Draw() {
+function draw() {
 	//drawCircle(player_x * pixels_in_meter, player_y * pixels_in_meter, size_of_player * pixels_in_meter, "black");
-	drawFilledCircle(screenWidth/2, screenHeight/2, size_of_player * pixels_in_meter, "green");
+	draw_filled_circle(screen_wid/2, screen_heit/2, size_of_player * pixels_in_meter, "green");
 	
-	var t0 = performance.now();
-
-	DrawMap();
-	
-	var t1 = performance.now();
-	console.log("Rendering one frame takes " + (t1 - t0) + " milliseconds.");
-
+	draw_map();
 }
 
 function player_left_top_block() {
 	left_top_x = player_x - size_of_player;
 	left_top_y = player_y - size_of_player;
 	
-	left_top_x_block = Math.floor(left_top_x / blockMeterWid);
-	left_top_y_block = Math.floor(left_top_y / blockMeterHeit);
+	left_top_x_block = Math.floor(left_top_x / block_meter_wid);
+	left_top_y_block = Math.floor(left_top_y / block_meter_heit);
 
 	return [left_top_x_block, left_top_y_block];
 }
@@ -71,108 +87,76 @@ function player_bottom_right_block() {
 	bottom_right_x = player_x + size_of_player;
 	bottom_right_y = player_y + size_of_player;
 	
-	bottom_right_x_block = Math.floor(bottom_right_x / blockMeterWid);
-	bottom_right_y_block = Math.floor(bottom_right_y / blockMeterHeit);
+	bottom_right_x_block = Math.floor(bottom_right_x / block_meter_wid);
+	bottom_right_y_block = Math.floor(bottom_right_y / block_meter_heit);
 	
 	return [bottom_right_x_block, bottom_right_y_block];
 }
 
-function Collision() {	
-	var left_top = player_left_top_block()
-	left_top_x_block = left_top[0];
-	left_top_y_block = left_top[1];
-	
-	var bottom_right = player_bottom_right_block();
-	bottom_right_x_block = bottom_right[0];
-	bottom_right_y_block = bottom_right[1];
-	
-	for (var i = left_top_x_block; i <= bottom_right_x_block; i++) {
-		for (var j = left_top_y_block; j <= bottom_right_y_block; j++) {
-			if (levelMap[i] == undefined) {
-				
-			}
-			else if (levelMap[i][j] == undefined) {
-				
-			}
-			else if (levelMap[i][j] != 0) {return j * blockMeterHeit;}
-		}
-	}
-	
-	return false;
-}
-
-function upKeyDown() {
+function up_key_down() {
 	if (!flying) {
 		speed_of_player_y -= max_speed_of_player;
 		flying = true; 
 	}
 }
 
-function leftKeyDown() {
+function left_key_down() {
 	speed_of_player_x = - max_speed_of_player;
 }
 
-function leftKeyUp() {
+function left_key_up() {
 	speed_of_player_x = 0;
 }
 
-function rightKeyDown() {
+function right_key_down() {
 	speed_of_player_x = max_speed_of_player;
 }
 
-function rightKeyUp() {
+function right_key_up() {
 	speed_of_player_x = 0;
 }
 
-function downKeyDown() {}
-
-function InitializeLevel()
+function initialize_level()
 {
-	var jMin = 0;
+	var j_min = 0;
 	var flag = 0;
-	var RandomWidth = 0;
-	for(var i = 0; i < levelWidth; i++)
+	var random_wid = 0;
+	for(var i = 0; i < level_wid; i++)
 	{
-		levelMap[i] = new Array(levelHeight);
+		level_map[i] = new Array(level_heit);
 
-		if(flag < RandomWidth)
+		if(flag < random_wid)
 		{
 			flag++;
 		} 
 		else
 		{
-			RandomWidth = randomInteger(2, 15);
-			jMin = randomInteger(36, 42);
+			random_wid = random_integer(2, 15);
+			j_min = random_integer(36, 42);
 			flag = 0;
 		}
 		
-		for(var j = 0; j < levelHeight; j++)
+		for(var j = 0; j < level_heit; j++)
 		{
-			//|| (j == 19 && i > 20 && i < 30)
-			if((j > jMin) && (i < 10 || i > 20))
+			if((j > j_min) && (i < 10 || i > 20))
 			{
-				levelMap[i][j] = 1;
+				level_map[i][j] = 1;
 			}
-			
-			/*else if (j==21 && i == 45) {
-				levelMap[i][j] = 1;
-			}*/
 			else
 			{
-				levelMap[i][j] = 0;
+				level_map[i][j] = 0;
 			}
 		}
 	}	
 }
 
-InitializeLevel();
-//console.log(levelMap);
+initialize_level();
 
-function DrawMap()
+function draw_map()
 {
 	left_top = player_left_top_block();
 	
-	var context = mainContext;
+	var context = main_context;
 	context.beginPath();
 	  
 	for(var k = -50; k < 50; k++)
@@ -182,21 +166,21 @@ function DrawMap()
 			var i = left_top[0] + k;
 			var j = left_top[1] + m;
 			
-			if (levelMap[i] == undefined) {
-				drawOpaqueRectangleOptimized(i * blockPixelWidth - (player_x * pixels_in_meter) + screenWidth/2,
-				j * blockPixelHeight - (player_y * pixels_in_meter) + screenHeight/2, 
-				blockPixelWidth - 1, blockPixelHeight - 1, 'red');
+			if (level_map[i] == undefined) {
+				draw_opaque_rectangle_optimized(i * block_pixel_wid - (player_x * pixels_in_meter) + screen_wid/2,
+				j * block_pixel_heit - (player_y * pixels_in_meter) + screen_heit/2, 
+				block_pixel_wid - 1, block_pixel_heit - 1, 'red');
 			}
-			else if (levelMap[i][j] == undefined) {
-				drawOpaqueRectangleOptimized(i * blockPixelWidth - (player_x * pixels_in_meter) + screenWidth/2,
-				j * blockPixelHeight - (player_y * pixels_in_meter) + screenHeight/2, 
-				blockPixelWidth - 1, blockPixelHeight - 1, 'red');
+			else if (level_map[i][j] == undefined) {
+				draw_opaque_rectangle_optimized(i * block_pixel_wid - (player_x * pixels_in_meter) + screen_wid/2,
+				j * block_pixel_heit - (player_y * pixels_in_meter) + screen_heit/2, 
+				block_pixel_wid - 1, block_pixel_heit - 1, 'red');
 			}
-			else if(levelMap[i][j] != 0)
+			else if(level_map[i][j] != 0)
 			{
-				drawOpaqueRectangleOptimized(i * blockPixelWidth - (player_x * pixels_in_meter) + screenWidth/2,
-				j * blockPixelHeight - (player_y * pixels_in_meter) + screenHeight/2, 
-				blockPixelWidth - 1, blockPixelHeight - 1);
+				draw_opaque_rectangle_optimized(i * block_pixel_wid - (player_x * pixels_in_meter) + screen_wid/2,
+				j * block_pixel_heit - (player_y * pixels_in_meter) + screen_heit/2, 
+				block_pixel_wid - 1, block_pixel_heit - 1);
 			}
 		}
 	}
